7ebd1dc90c03a78f26ed67cea7c7c227511de536
Sahibdeep Singh Sodhi <mssodhi11@gmail.com>
2025-03-02 23:57:58 +0530

    Added comments for 3471. Find the Largest Almost Missing Integer

--- 3471. Find the Largest Almost Missing Integer.java
+++ 3471. Find the Largest Almost Missing Integer.java
@@ -8,17 +8,20 @@ class Solution {
         int largestMissing = -1;
         Map<Integer, Integer> frequency = new HashMap<>();
 
+        //Store unique elements in each subarray of size k
         for(int i = 0; i <= n-k; i++){
             HashSet<Integer> unique = new HashSet<>();
             for(int j = i; j < i + k; j++){
                 unique.add(nums[j]);
             }
 
+            //Count frequency of unique elements
             for(Integer num : unique){
                 frequency.put(num, frequency.getOrDefault(num, 0) + 1);
             }
         }
 
+        //Find the largest missing integer
         for(Integer num : frequency.keySet()){
             if(frequency.get(num) == 1){
                 largestMissing = Math.max(largestMissing, num);
