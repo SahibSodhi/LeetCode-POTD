1e065323e30dd2d0f8bed287b9e58fea127a7928
SahibSodhi <mssodhi11@gmail.com>
2025-06-06 23:14:57 +0530

    Implement solution for 2434. Using a Robot to Print the Lexicographically Smallest String

---
+++ 2434. Using a Robot to Print the Lexicographically Smallest String.java
@@ -0,0 +1,26 @@
+class Solution {
+    //Approach - Greedy + Stack
+    //TC: O(n + |S|), SC: O(n); Let n be the length of the string s, and let |S| // denote the size of the character set.
+    public String robotWithString(String s) {
+        int[] cnt = new int[26];
+        for (char c : s.toCharArray()) {
+            cnt[c - 'a']++;
+        }
+
+        Stack<Character> stack = new Stack<>();
+        StringBuilder res = new StringBuilder();
+        char minCharacter = 'a';
+        for (char c : s.toCharArray()) {
+            stack.push(c);
+            cnt[c - 'a']--;
+            while (minCharacter != 'z' && cnt[minCharacter - 'a'] == 0) {
+                minCharacter++;
+            }
+            while (!stack.isEmpty() && stack.peek() <= minCharacter) {
+                res.append(stack.pop());
+            }
+        }
+
+        return res.toString();
+    }
+}
